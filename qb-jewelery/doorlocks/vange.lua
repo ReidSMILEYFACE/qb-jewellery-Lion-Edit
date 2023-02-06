4328a76b36f5632144e53019205ea48ba6a3860f
ReidSMILEYFACE <104175119+ReidSMILEYFACE@users.noreply.github.com>
2023-02-06 20:41:15 +0000

    added doorlocks

---
+++ qb-jewelery/doorlocks/vange.lua
@@ -0,0 +1,15 @@
+
+
+-- vange created by Shrek
+Config.DoorList['vange-vange'] = {
+    doors = {
+        {objName = 1425919976, objYaw = 306.00003051758, objCoords = vec3(-631.955383, -236.333267, 38.206532)},
+        {objName = 9467943, objYaw = 306.00003051758, objCoords = vec3(-630.426514, -238.437546, 38.206532)}
+    },
+    doorLabel = 'vange',
+    doorRate = 1.0,
+    locked = true,
+    doorType = 'double',
+    distance = 2,
+    authorizedJobs = { ['police'] = 0 },
+}
---
+++ qb-jewelery/doorlocks/vange2.lua
@@ -0,0 +1,15 @@
+
+
+-- vange2 created by Shrek
+Config.DoorList['vange2-vange2'] = {
+    objYaw = 36.000022888184,
+    objName = 1335309163,
+    distance = 2,
+    locked = true,
+    fixText = false,
+    doorType = 'door',
+    authorizedJobs = { ['police'] = 0 },
+    doorRate = 1.0,
+    objCoords = vec3(-629.133850, -230.151703, 38.206585),
+    doorLabel = 'vange2',
+}
